a235a8be528beb57a3b6829e51c4f251c8daa44d
Christian Allen <rhen10@gmail.com>
2020-05-02 01:36:52 -0700

    Setup data binding template + changed app startup

--- WGUCapstoneProject/WGUCapstoneProject/App.xaml.cs
+++ WGUCapstoneProject/WGUCapstoneProject/App.xaml.cs
@@ -13,5 +13,10 @@ namespace WGUCapstoneProject
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            DataBindingWindow dbWindow = new DataBindingWindow();
+            dbWindow.Show();
+        }
     }
 }
