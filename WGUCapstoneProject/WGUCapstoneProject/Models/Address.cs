05ccacaa8e7a9999aaf030d88f5ee54e1048e30e
Christian Allen <rhen10@gmail.com>
2020-05-02 02:15:59 -0700

    Address class made public

--- WGUCapstoneProject/WGUCapstoneProject/Models/Address.cs
+++ WGUCapstoneProject/WGUCapstoneProject/Models/Address.cs
@@ -4,7 +4,13 @@ using System.Text;
 
 namespace WGUCapstoneProject.Models
 {
-    class Address
+    public class Address
     {
+        public int AddressId { get; set; }
+        public int AddressNumber { get; set; }
+        public string StreetName { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
     }
 }
